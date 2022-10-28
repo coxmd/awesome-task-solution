105850c4da421873be2fc01ac58d49caf7315089
coxMD <coxmusyoki@gmail.com>
2022-10-28 20:23:47 +0300

    completed question 9

--- main.js
+++ main.js
@@ -179,7 +179,15 @@ console.log(obj.perimeter([45, 56, 67])); // expected output: 168
 const fizzBuzz = () => {
     for(let i = 0; i <= 21; i++) {
         // write your code here...
-
+        if ( i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        }else {
+            console.log(i);
+        }
     }
 }
 
